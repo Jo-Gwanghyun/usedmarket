f04c625681f395c57008872da63597260ccef1cd
Jo-Gwanghyun <mhc0404@naver.com>
2024-11-03 02:12:07 +0900

    메인페이지 업로드

---
+++ src/test/java/com/usedmarket/UsedmarketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.usedmarket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UsedmarketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
